3f8697e799e05fba25c5c36c393d6e2d95e93d02
arsaizdihar <arsadihar@gmail.com>
2024-04-27 10:19:35 +0700

    feat: upgrade pnpm, add sparta link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,9 +9,14 @@ function Index() {
   return (
     <div className="">
       <h3 className="p-2 text-2xl font-bold">Welcome Home!</h3>
-      <button onClick={setupNotification} className="rounded bg-blue-400 p-2">
-        Subscribe to push notif
-      </button>
+      <div className="flex flex-col items-start space-y-2">
+        <button onClick={setupNotification} className="rounded bg-blue-400 p-2">
+          Subscribe to push notif
+        </button>
+        <a className="rounded bg-blue-400 p-2" href="/sparta">
+          SPARTA
+        </a>
+      </div>
     </div>
   );
 }
