0fd8416bba2efad6559db8c20669d0eaaf287863
rma1403 <ravamaulana14@gmail.com>
2024-04-29 00:23:39 +0700

    feat: update text field component

--- src/components/ui/textfield.tsx
+++ src/components/ui/textfield.tsx
@@ -1,6 +1,5 @@
 import { cva, VariantProps } from 'class-variance-authority';
 import * as React from 'react';
-import { FieldError } from 'react-hook-form';
 
 import { cn } from '~/lib/utils';
 
@@ -10,36 +9,14 @@ import CheckIcon from '~/assets/icons/textfield/check.svg';
 import WarningIcon from '~/assets/icons/textfield/warning.svg';
 
 const textFieldVariants = cva(
-  'flex w-full border-none bg-background text-sm text-[#020617] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#64748B] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-  {
-    variants: {
-      variant: {
-        default: '',
-        search:
-          'bg-[#F8FAFC] focus-visible:bg-[#F1F5F9] group-hover:bg-[#F1F5F9]',
-      },
-      fieldSize: {
-        default: '',
-        m: '',
-        xl: '',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-      fieldSize: 'default',
-    },
-  },
-);
-
-const divVariants = cva(
-  'group flex w-full items-center gap-2 rounded-lg border',
+  'flex w-full rounded-lg border bg-background text-sm text-[#020617] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#64748B] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
       variant: {
         default:
-          'border-[#E2E8F0] focus-within:border-[#94A3B8] hover:border-[#CBD5E1]',
+          'border-[#E2E8F0] hover:border-[#CBD5E1] focus-visible:border-[#94A3B8]',
         search:
-          'border-[#F8FAFC] bg-[#F8FAFC] focus-within:border-[#F1F5F9] focus-within:bg-[#F1F5F9] hover:border-[#F1F5F9] hover:bg-[#F1F5F9]',
+          'border-[#F8FAFC] bg-[#F8FAFC] !pl-12 hover:border-[#F1F5F9] hover:bg-[#F1F5F9] focus-visible:border-[#F1F5F9] focus-visible:bg-[#F1F5F9] group-hover:bg-[#F1F5F9]',
       },
       fieldSize: {
         default: 'px-4 py-3',
@@ -56,9 +33,8 @@ const divVariants = cva(
 
 export interface TextFieldProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
-    VariantProps<typeof textFieldVariants>,
-    VariantProps<typeof divVariants> {
-  error?: FieldError;
+    VariantProps<typeof textFieldVariants> {
+  error?: string;
   success?: boolean;
 }
 
@@ -69,7 +45,7 @@ export interface TextFieldProps
  * @param variant - "default" or "search"
  * @param fieldSize - "default", "m", or "xl
  * @param [success] - Indicates if the input is successfully submitted.
- * @param [error] - React-hook-form error object.
+ * @param [error] - Error message.
  */
 const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
   (
@@ -77,36 +53,43 @@ const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
     ref,
   ) => {
     return (
-      <div className="flex flex-col gap-2 font-inter">
-        <div
-          className={cn(
-            props.disabled && '!border-[#E2E8F0]',
-            divVariants({ className, variant, fieldSize }),
+      <div className={cn('flex flex-col gap-2 font-inter', className)}>
+        <div className={cn(props.disabled && '!border-[#E2E8F0]', 'relative')}>
+          {variant === 'search' && (
+            <img
+              src={SearchIcon}
+              className="absolute left-4 top-1/2 size-4 -translate-y-1/2"
+            />
           )}
-        >
-          {variant === 'search' && <img src={SearchIcon} className="size-4" />}
           <input
             type={type}
             className={cn(
-              textFieldVariants({ variant }),
+              textFieldVariants({ variant, fieldSize }),
               error ? '!text-[#FF2C20]' : '',
+              props.disabled || error || success || children
+                ? children && (props.disabled || error || success)
+                  ? 'pr-16'
+                  : 'pr-9'
+                : '',
             )}
             ref={ref}
             {...props}
           />
-          {(props.disabled || error || success) && (
-            <img
-              src={
-                props.disabled ? WarningIcon : success ? CheckIcon : AlertIcon
-              }
-              className="size-4"
-            />
-          )}
-          {children}
+          <div className="absolute right-3 top-1/2 flex -translate-y-1/2 items-center gap-2">
+            {(props.disabled || error || success) && (
+              <img
+                src={
+                  props.disabled ? WarningIcon : success ? CheckIcon : AlertIcon
+                }
+                className="size-4"
+              />
+            )}
+            {children}
+          </div>
         </div>
 
         {!props.disabled && error && (
-          <p className="text-sm text-[#FF2C20]">* {error.message}</p>
+          <p className="text-sm text-[#FF2C20]">* {error}</p>
         )}
       </div>
     );
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,66 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { Button } from '~/components/ui/button';
+import { Form, FormControl, FormField, FormItem } from '~/components/ui/form';
+import { TextField } from '~/components/ui/textfield';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+// import SearchIcon from '~/assets/icons/textfield/search.svg';
 
 export const Route = createFileRoute('/about')({
   component: About,
 });
 
 function About() {
-  return <div className="p-2">Hello from About!</div>;
+  const FormSchema = z.object({
+    username: z.string().min(5, {
+      message: 'Username must be atleast 5 characters.',
+    }),
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="p-2">
+      Hello from About!
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <TextField
+                    className="mt-10 w-3/4"
+                    variant="search"
+                    placeholder="TextField"
+                    error={form.formState.errors.username?.message}
+                    success={form.formState.isSubmitSuccessful}
+                    // fieldSize="m"
+                    {...field}
+                  >
+                    {/* <img src={SearchIcon} className="size-4" /> */}
+                  </TextField>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="mt-10">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
