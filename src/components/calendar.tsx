467d156c71a905b3216dbc615edaa2e9b6b1db62
arsaizdihar <arsadihar@gmail.com>
2024-05-21 21:38:00 +0700

    feat: change calendar background

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { useState } from 'react';
 import { generateDate, months } from '../lib/calendar';
 import { cn } from '../lib/utils';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 export default function Calendar() {
   const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
@@ -10,9 +10,13 @@ export default function Calendar() {
   const [today, setToday] = useState(currentDate);
   const [selectDate, setSelectDate] = useState(currentDate);
   return (
-    <div className="flex size-full flex-col items-center justify-center sm:flex-row">
-      <div className="flex flex-col gap-4 rounded-xl bg-green-300 p-8">
-        <div className="flex items-center justify-between">
+    <div className="flex w-full flex-col items-center justify-center sm:flex-row">
+      <div className="relative flex flex-col gap-4 rounded-xl p-8">
+        <img
+          src="/main-dashboard/calendar-bg.png"
+          className="absolute left-0 top-0 size-full select-none rounded-xl object-cover"
+        />
+        <div className="relative flex items-center justify-between">
           <div className="flex w-full items-center justify-around gap-10">
             <ChevronLeft
               className="cursor-pointer text-yellow-300 transition-all hover:scale-105"
@@ -39,20 +43,20 @@ export default function Calendar() {
             />
           </div>
         </div>
-        <div className="grid grid-cols-7">
+        <div className="relative grid grid-cols-7">
           {days.map((day, index) => {
             return (
-              <h1
+              <div
                 key={index}
                 className="grid select-none place-content-center text-center text-sm font-semibold text-gray-300"
               >
                 {day}
-              </h1>
+              </div>
             );
           })}
         </div>
 
-        <div className="grid grid-cols-7">
+        <div className="relative grid grid-cols-7">
           {generateDate(today.month(), today.year()).map(
             ({ date, currentMonth, today }, index) => {
               return (
@@ -62,7 +66,7 @@ export default function Calendar() {
                 >
                   <h1
                     className={cn(
-                      currentMonth ? '' : 'text-gray-500',
+                      currentMonth ? '' : 'text-gray-400',
                       today ? 'bg-green-100' : '',
                       selectDate.toDate().toDateString() ===
                         date.toDate().toDateString()
