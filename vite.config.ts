cabb167fa23550a6ecb1d449a1cd8bd1752acc2d
arsaizdihar <arsadihar@gmail.com>
2024-05-21 23:00:54 +0700

    feat: change all icons

--- vite.config.ts
+++ vite.config.ts
@@ -32,14 +32,21 @@ export default defineConfig({
             type: 'image/png',
           },
           {
-            src: '/android-chrome-384x384.png',
-            sizes: '384x384',
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/android-512x512.png',
+            src: '/android-chrome-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/android-chrome-maskable-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'maskable',
           },
         ],
         screenshots: [
