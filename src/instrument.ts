690d5d2d0e645bc3d76c6b051676db9883db440a
arsaizdihar <arsadihar@gmail.com>
2024-06-15 13:14:32 +0700

    feat: update packages, integrate more for sentry

--- src/instrument.ts
+++ src/instrument.ts
@@ -3,9 +3,12 @@ import { router } from './router';
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.VITE_SENTRY_ENVIRONMENT ?? 'development',
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.tanstackRouterBrowserTracingIntegration(router),
   ],
   tracesSampleRate: 1.0,
 });
+
+export default Sentry;
