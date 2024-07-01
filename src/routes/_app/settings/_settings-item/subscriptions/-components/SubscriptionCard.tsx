76168611dd1b83d307c71ca077a713d38495dfd1
18221171 <18221171@std.stei.itb.ac.id>
2024-07-01 11:49:34 +0700

    feat: connect subscription page to api

--- src/routes/_app/settings/_settings-item/subscriptions/-components/SubscriptionCard.tsx
+++ src/routes/_app/settings/_settings-item/subscriptions/-components/SubscriptionCard.tsx
@@ -1,33 +1,36 @@
 import { Switch } from '~/components/ui/switch';
 
-export interface SubscriptionData {
-  imageSrc: string;
+export interface CategoryData {
+  id: string;
   name: string;
-  active: boolean;
+  requiredPush: boolean;
 }
 
 interface SubscriptionCardProps {
-  subscriptionData: SubscriptionData;
+  categoryData: CategoryData;
+  unsubscribed: boolean;
   onSwitchChange: (name: string, active: boolean) => void;
 }
 
 export default function SubscriptionCard({
-  subscriptionData,
+  categoryData,
+  unsubscribed,
   onSwitchChange,
 }: SubscriptionCardProps) {
   const handleSwitchChange = (checked: boolean) => {
-    onSwitchChange(subscriptionData.name, checked);
+    onSwitchChange(categoryData.id, checked);
   };
   return (
     <div className="relative flex w-full items-center rounded-xl">
       <div className="z-10 flex w-full items-center justify-between rounded-xl bg-white transition-all duration-300 ease-out">
         <div className="flex items-center gap-4 px-5 py-4">
-          <img src={subscriptionData.imageSrc} alt="Logo" className="w-10" />
-          <p className="font-bold leading-5">{subscriptionData.name}</p>
+          <img src="/logo/hmif.png" alt="Logo" className="w-10" />
+          <p className="font-bold leading-5">{categoryData.name}</p>
         </div>
         <div className="px-5 py-4">
           <Switch
-            checked={subscriptionData.active}
+            disabled={categoryData.requiredPush}
+            checked={!unsubscribed}
             onCheckedChange={handleSwitchChange}
           />
         </div>
