f539c4f73b3bd32da968c78c2cf3031628bfa06c
arsaizdihar <arsadihar@gmail.com>
2024-08-05 13:07:21 +0700

    feat: tidy up some uis

--- src/routes/_app/timeline/$infoId/-components/post-photos.tsx
+++ src/routes/_app/timeline/$infoId/-components/post-photos.tsx
@@ -3,11 +3,11 @@ const PostPhotos = ({ images }: { images: string[] }) => {
     <div className="mt-5 w-full">
       {images.length === 1 ? (
         <div className="grid grid-cols-1 gap-4">
-          <div className="relative border">
+          <div className="relative">
             <img
               src={images[0]}
               alt="Post"
-              className="size-full rounded-lg object-cover"
+              className="size-full max-w-96 rounded-lg border object-cover"
             />
           </div>
         </div>
@@ -17,14 +17,14 @@ const PostPhotos = ({ images }: { images: string[] }) => {
             <img
               src={images[0]}
               alt="Post"
-              className="size-full rounded-l-lg border object-cover"
+              className="size-full max-w-96 rounded-l-lg border object-cover"
             />
           </div>
           <div className="relative w-1/2">
             <img
               src={images[1]}
               alt="Post"
-              className="size-full rounded-r-lg border object-cover"
+              className="size-full max-w-96 rounded-r-lg border object-cover"
             />
           </div>
         </div>
@@ -33,7 +33,7 @@ const PostPhotos = ({ images }: { images: string[] }) => {
           <img
             src={images[0]}
             alt="Post"
-            className="row-span-2 size-full rounded-l-lg border object-cover"
+            className="row-span-2 size-full max-w-96 rounded-l-lg border object-cover"
           />
           <img
             src={images[1]}
@@ -63,7 +63,7 @@ const PostPhotos = ({ images }: { images: string[] }) => {
             className="h-96 w-full rounded-bl-lg border object-cover"
             src={images[2]}
           />
-          <div className="relative size-full rounded-br-lg border text-center">
+          <div className="relative size-full max-w-96 rounded-br-lg border text-center">
             <img
               alt="Post"
               className="h-96 w-full rounded-br-lg border object-cover brightness-50"
