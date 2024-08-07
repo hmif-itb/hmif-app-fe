cb421cd072dbfac213a032a4009b22287d27ee66
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-08-07 19:25:10 +0700

    feat: added event form for mobile

---
+++ src/routes/_app/add-event/-components/description.tsx
@@ -0,0 +1,27 @@
+import { FormField, FormItem, FormControl } from '~/components/ui/form';
+import { ComponentProps } from '../-constants';
+import { GrowingTextarea } from '~/components/ui/growingtextarea';
+
+export default function Description({ form }: ComponentProps): JSX.Element {
+  return (
+    <div>
+      <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <GrowingTextarea
+                maxLength={500}
+                placeholder="Description"
+                iconClassName="!size-3 !top-auto bottom-1 !translate-y-0 !right-14"
+                className="outline-none"
+                {...field}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+}
