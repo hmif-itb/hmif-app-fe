8d5900e1197998be6036211cbbb11fdc609512f8
rma1403 <ravamaulana14@gmail.com>
2024-08-17 18:03:42 +0700

    feat: test add anim

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,10 +3,16 @@ import SettingsIcon from '../icons/settings';
 import BottomNavigationItem from './bottom-navigation-item';
 // import AddEvent from '~/routes/_app/add-event/add-event';
 import HomeFilledIcon from '../icons/home-filled';
+import { motion } from 'framer-motion';
 
 function Navbar() {
   return (
-    <div className="sticky bottom-0 z-20 flex flex-col justify-end">
+    <motion.div
+      initial={{ bottom: -100 }}
+      animate={{ bottom: 0 }}
+      transition={{ duration: 1 }}
+      className="sticky z-20 flex flex-col justify-end"
+    >
       <nav className="flex w-full justify-between bg-neutral-light px-8 py-3 lg:hidden">
         <BottomNavigationItem
           to="/timeline"
@@ -29,7 +35,7 @@ function Navbar() {
           title="Settings"
         />
       </nav>
-    </div>
+    </motion.div>
   );
 }
 
