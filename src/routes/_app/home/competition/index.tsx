e1eb81edc3f6af9e667bd158a9dd35c0c3fdf66e
rma1403 <ravamaulana14@gmail.com>
2024-08-26 00:38:37 +0700

    feat: added desktop comp page

--- src/routes/_app/home/competition/index.tsx
+++ src/routes/_app/home/competition/index.tsx
@@ -1,5 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
-import HeaderTitle from '~/components/header-title';
+import { createFileRoute, useRouter } from '@tanstack/react-router';
 import CompetitionCard from './-components/CompetitionCard';
 import { Button } from '~/components/ui/button';
 import PlusIcon from '~/assets/icons/plus.svg';
@@ -10,6 +9,7 @@ import { api } from '~/api/client';
 import { InView } from 'react-intersection-observer';
 import { AddCompetitionDrawer } from './-components/AddCompetitionDrawer';
 import AddCompetitionDialog from './-components/AddCompetitionDialog';
+import { ChevronLeft } from 'lucide-react';
 
 export const Route = createFileRoute('/_app/home/competition/')({
   component: CompetitionPage,
@@ -20,7 +20,9 @@ function CompetitionPage() {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [dialogOpen, setDialogOpen] = useState(false);
 
-  const constraintRef = useRef<HTMLElement>(null);
+  const constraintRef = useRef<HTMLDivElement>(null);
+
+  const router = useRouter();
 
   const {
     data: competitions,
@@ -57,63 +59,72 @@ function CompetitionPage() {
     fetchNextPage();
 
   return (
-    <div className="flex size-full h-screen flex-col overflow-hidden">
-      <div className="hidden w-full bg-white lg:block">
-        <HeaderTitle />
-      </div>
-
-      <main
+    <div className="flex size-full h-screen flex-col overflow-hidden bg-green-50">
+      <div
+        className="relative mx-auto flex w-full flex-col items-center lg:max-w-screen-lg lg:px-8"
         ref={constraintRef}
-        className="relative flex size-full max-h-screen flex-col items-center gap-3 overflow-hidden bg-[url(/img/login/login-bg.jpg)] px-8 md:bg-[url(/img/login/login-bg-desktop.jpg)] lg:px-[52px]"
       >
-        <AddCompetitionDialog
-          isOpen={dialogOpen}
-          setOpen={setDialogOpen}
-          constraintRef={constraintRef}
-        />
-
-        <div className="mt-[50px] flex w-full items-center justify-between">
-          <h1 className="text-left text-4xl font-bold text-white lg:text-5xl">
-            Competition
-          </h1>
-
-          <Button
-            onClick={() => setDialogOpen(true)}
-            className="hidden bg-yellow-300 px-3 py-2 text-xs lg:flex"
-          >
-            Add New Competition
-            <img src={PlusCircle} className="size-4" alt="" />
-          </Button>
-        </div>
-
-        <ul className="flex flex-col gap-4 overflow-auto pb-16 pt-6">
-          {competitions?.pages
-            .flatMap((c) => c)
-            .map((competition, idx) =>
-              idx < competitions?.pages.flatMap((c) => c).length - 2 ? (
-                <li key={competition.id}>
-                  <CompetitionCard {...competition} />
-                </li>
-              ) : (
-                <InView
-                  key={competition.id}
-                  onChange={(inView) => inView && fetchWhenInView()}
-                >
-                  <CompetitionCard {...competition} />
-                </InView>
-              ),
-            )}
-        </ul>
-
         <Button
-          onClick={() => setDrawerOpen(true)}
-          className="fixed bottom-24 right-5 z-20 rounded-full border border-green-300 bg-yellow-75 p-5 lg:hidden"
+          variant="link"
+          className="my-6 hidden w-full justify-start gap-8 p-0 text-3xl font-medium lg:flex"
+          onClick={() => {
+            router.history.back();
+          }}
         >
-          <img src={PlusIcon} className="size-8" alt="" />
+          <ChevronLeft className="size-8" />
+          <span>Back</span>
         </Button>
 
-        <AddCompetitionDrawer isOpen={drawerOpen} setOpen={setDrawerOpen} />
-      </main>
+        <main className="flex size-full max-h-screen flex-col items-center gap-3 overflow-hidden bg-[url(/img/login/login-bg.jpg)] bg-no-repeat px-8 md:bg-[url(/img/login/login-bg-desktop.jpg)] lg:max-w-screen-lg lg:rounded-2xl lg:px-[82px]">
+          <AddCompetitionDialog
+            isOpen={dialogOpen}
+            setOpen={setDialogOpen}
+            constraintRef={constraintRef}
+          />
+
+          <div className="mt-[50px] flex w-full items-center justify-between lg:mt-8">
+            <h1 className="text-left text-4xl font-bold text-white lg:text-5xl">
+              Competition
+            </h1>
+
+            <Button
+              onClick={() => setDialogOpen(true)}
+              className="hidden bg-yellow-300 px-3 py-2 text-xs lg:flex"
+            >
+              Add New Competition
+              <img src={PlusCircle} className="size-4" alt="" />
+            </Button>
+          </div>
+
+          <ul className="flex flex-col gap-4 overflow-auto pb-28 pt-6 lg:grid lg:grid-cols-2 lg:gap-10 lg:px-4 lg:pt-3">
+            {competitions?.pages
+              .flatMap((c) => c)
+              .map((competition, idx) =>
+                idx < competitions?.pages.flatMap((c) => c).length - 2 ? (
+                  <li key={competition.id}>
+                    <CompetitionCard {...competition} />
+                  </li>
+                ) : (
+                  <InView
+                    key={competition.id}
+                    onChange={(inView) => inView && fetchWhenInView()}
+                  >
+                    <CompetitionCard {...competition} />
+                  </InView>
+                ),
+              )}
+          </ul>
+
+          <Button
+            onClick={() => setDrawerOpen(true)}
+            className="fixed bottom-24 right-5 z-20 rounded-full border border-green-300 bg-yellow-75 p-5 lg:hidden"
+          >
+            <img src={PlusIcon} className="size-8" alt="" />
+          </Button>
+
+          <AddCompetitionDrawer isOpen={drawerOpen} setOpen={setDrawerOpen} />
+        </main>
+      </div>
     </div>
   );
 }
