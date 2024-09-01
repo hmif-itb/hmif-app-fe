bdf5225f0afb64ef2a1f48ebe6a8d6a97a8cdadf
rma1403 <ravamaulana14@gmail.com>
2024-09-01 18:09:51 +0700

    fix: scrollable setting

--- src/routes/_app/_left-navbar/settings.tsx
+++ src/routes/_app/_left-navbar/settings.tsx
@@ -25,8 +25,8 @@ function SettingsLayout() {
   );
   const { logout } = useLogout();
   return (
-    <div className="flex h-full flex-col overflow-hidden">
-      <div className="flex-1 overflow-y-auto bg-green-50">
+    <div className="flex h-screen flex-col overflow-hidden">
+      <div className="flex-1 overflow-hidden bg-green-50">
         <div className="mx-auto mb-10 flex h-full max-w-screen-lg flex-col lg:px-8">
           <h1 className="my-6 hidden items-center space-x-12 text-heading-lg font-bold lg:flex">
             {selectedSettings && (
