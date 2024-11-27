911a2057af697ddda50711849137cee748de8736
WwzFwz <dzakyaureliafawwaz@gmail.com>
2024-11-27 23:51:09 +0700

    feat added reccomendation detail header

---
+++ src/routes/_app/_left-navbar/home/rekomendasi/$rekomendasiId/index.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_app/_left-navbar/home/rekomendasi/$rekomendasiId/')({
+  component: () => <div>Hello /_app/_left-navbar/home/rekomendasi/$rekomendasiId/!</div>
+})
