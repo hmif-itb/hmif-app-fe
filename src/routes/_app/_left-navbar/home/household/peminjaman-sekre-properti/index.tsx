286714f59fe8145e3a6ebe13b9410665c72d9070
Shafiq Irvansyah <68421940+shafiqIrv@users.noreply.github.com>
2025-09-20 11:04:12 +0700

    feat: create peminjaman sekre and property page

---
+++ src/routes/_app/_left-navbar/home/household/peminjaman-sekre-properti/index.tsx
@@ -0,0 +1,129 @@
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { useState, useEffect } from 'react';
+import { Button } from '~/components/ui/button';
+import { ChevronLeft } from 'lucide-react';
+import { useRouter } from '@tanstack/react-router';
+import SearchBar from './-components/SearchBar';
+import PropertyList from './-components/PropertyList';
+import SekreList from './-components/SekreList';
+import { SwitchToggle } from './-components/Switch';
+import { FilterOptions } from './-components/FilterModal';
+import { isInRoles } from '~/lib/roles';
+import { loadUserCache } from '~/lib/session';
+
+export const Route = createFileRoute(
+  '/_app/_left-navbar/home/household/peminjaman-sekre-properti/',
+)({
+  component: HouseholdAdminPage,
+  //   loader: () => {
+  //     if (!loadUserCache!()) {
+  //       throw redirect({ to: '/home/household' });
+  //     }
+  //     if (loadUserCache()) {
+  //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //       // @ts-expect-error
+  //       if (!isInRoles(loadUserCache(), ['household'])) {
+  //         throw redirect({ to: '/home/household' });
+  //       }
+  //     }
+  //   },
+});
+
+function HouseholdAdminPage() {
+  const router = useRouter();
+  const [activeView, setActiveView] = useState('Properti');
+  const [isMobile, setIsMobile] = useState(false);
+  const [filter, setFilter] = useState<FilterOptions>({ condition: 'all' });
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+
+    return () => window.removeEventListener('resize', checkIfMobile);
+  }, []);
+
+  const handleSwitchChange = (value: string) => {
+    console.log('Selected:', value);
+    setActiveView(value);
+  };
+
+  const handleFilterChange = (newFilter: FilterOptions) => {
+    setFilter(newFilter);
+  };
+
+  const handleSearchChange = (newSearchTerm: string) => {
+    setSearchTerm(newSearchTerm);
+  };
+
+  const renderContent = () => {
+    switch (activeView) {
+      case 'Properti':
+        return <PropertyList filter={filter} searchTerm={searchTerm} />;
+      case 'Sekre':
+        return <SekreList filter={filter} searchTerm={searchTerm} />;
+      default:
+        return <PropertyList filter={filter} searchTerm={searchTerm} />;
+    }
+  };
+
+  const mobileStyles = {
+    backgroundImage: `url('/img/household/mask-mobile.svg')`,
+    backgroundPosition: 'left top',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100% auto',
+  };
+
+  const desktopStyles = {
+    backgroundImage: `url('/img/household/mask-left-top.png'), url('/img/household/mask-right-bottom.png')`,
+    backgroundPosition: 'left top, right bottom',
+    backgroundRepeat: 'no-repeat, no-repeat',
+    backgroundSize: 'auto 1000px, auto 730px',
+  };
+
+  return (
+    <div className="flex h-full flex-col lg:px-10 lg:pb-[60px]">
+      {/* Back Button */}
+      <Button
+        variant="link"
+        className="my-6 hidden w-full justify-start gap-8 p-0 text-3xl font-medium lg:flex"
+        onClick={() => {
+          router.history.back();
+        }}
+      >
+        <ChevronLeft className="size-8" />
+        <span>Back</span>
+      </Button>
+      <main
+        className="flex size-full flex-col items-center gap-3 overflow-y-scroll bg-[#30764B] p-[34px] lg:gap-6 lg:rounded-xl lg:px-[26px]"
+        style={isMobile ? mobileStyles : desktopStyles}
+      >
+        <h1 className="flex items-center gap-6 text-[32px] font-bold text-white lg:text-center lg:text-5xl ">
+          <ChevronLeft
+            className="size-16 lg:hidden"
+            onClick={() => {
+              router.history.back();
+            }}
+          />
+          Peminjaman
+        </h1>
+        <SearchBar
+          onFilterChange={handleFilterChange}
+          onSearchChange={handleSearchChange}
+          currentFilter={filter}
+          searchTerm={searchTerm}
+        />
+        <SwitchToggle
+          options={['Properti', 'Sekre']}
+          defaultValue="Properti"
+          onValueChange={handleSwitchChange}
+        />
+        {renderContent()}
+      </main>
+    </div>
+  );
+}
