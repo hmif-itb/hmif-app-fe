dc49266bcb5311554dac715fe1e2fcdbbaa72364
WwzFwz <dzakyaureliafawwaz@gmail.com>
2025-10-10 05:41:20 +0700

    feat update  api

---
+++ src/api/generated/models/Prestasi.ts
@@ -0,0 +1,22 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type Prestasi = {
+  id: string;
+  userId: string;
+  jenisPrestasi: 'organisasi' | 'kepanitiaan' | 'kompetisi';
+  penyelenggara: string;
+  deskripsi: string | null;
+  bulan: number;
+  tahun: number;
+  competitionType: 'CP' | 'CTF' | 'BCC' | 'DS' | 'AI' | 'Hackathon' | null;
+  createdAt: string;
+  user?: {
+    id: string;
+    nim: string;
+    fullName: string;
+    picture: string | null;
+  } | null;
+};
+
